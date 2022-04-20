4efe5fe128c6af7caf40e08ffe2177cd28eefb20
Sergei Lavrentev <23312691+lavrxxx@users.noreply.github.com>
2022-04-20 16:38:19 +0300

    add random graph generation for bellman ford testing

--- algorithms/graph/src/lib.rs
+++ algorithms/graph/src/lib.rs
@@ -2,8 +2,8 @@ use std::cell::RefCell;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::rc::Rc;
 
-type NodeName = &'static str;
-type Weight = i8;
+type NodeName = String;
+type Weight = i128;
 type Link<T> = Rc<RefCell<Node<T>>>;
 type Edge<T> = (Link<T>, Weight);
 
@@ -45,7 +45,7 @@ where
             return;
         }
         act(self);
-        seen.insert(self.name);
+        seen.insert(self.name.clone());
         for edge in &self.edges {
             edge.0.borrow().traverse_depth_first(act, seen);
         }
@@ -66,7 +66,7 @@ where
             if let ActResult::Stop = res {
                 return;
             };
-            seen.insert(node.name);
+            seen.insert(node.name.clone());
             for edge in &node.edges {
                 stack.push_back(edge.clone())
             }
@@ -79,7 +79,7 @@ where
     T: Clone,
 {
     fn from(n: &Node<T>) -> Self {
-        let name = n.name;
+        let name = n.name.clone();
         let data = n.data.clone();
         let edges = n.edges.clone();
         Self { name, data, edges }
@@ -158,7 +158,7 @@ fn visit<T>(
     marked: &mut HashMap<NodeName, (Link<T>, bool)>,
     sorted: &mut VecDeque<Link<T>>,
 ) {
-    let marked_node: Option<&(Link<T>, bool)> = marked.get(node.borrow().name);
+    let marked_node: Option<&(Link<T>, bool)> = marked.get(&node.borrow().name);
     // If node is already marked.
     if let Some(node) = marked_node {
         // If node marked as permanent.
@@ -170,13 +170,13 @@ fn visit<T>(
         panic!("not a DAG")
     }
     // Mark node as temporary.
-    marked.insert(node.borrow().name, (node.clone(), false));
+    marked.insert(node.borrow().name.clone(), (node.clone(), false));
     for edge in &node.borrow().edges {
         // Visit neighbors.
         visit(edge.0.clone(), marked, sorted)
     }
     // Mark node as permanent.
-    marked.insert(node.borrow().name, (node.clone(), true));
+    marked.insert(node.borrow().name.clone(), (node.clone(), true));
     // Add node as sorted node.
     sorted.push_front(node);
 }
@@ -192,33 +192,33 @@ where
 
     root.borrow().traverse_breadth_first(
         &|edge| -> ActResult {
-            nodes.borrow_mut().insert(edge.0.borrow().name, edge.0.clone());
-            costs.borrow_mut().insert(edge.0.borrow().name, Weight::MAX);
-            parents.borrow_mut().insert(edge.0.borrow().name, None);
+            nodes.borrow_mut().insert(edge.0.borrow().name.clone(), edge.0.clone());
+            costs.borrow_mut().insert(edge.0.borrow().name.clone(), Weight::MAX);
+            parents.borrow_mut().insert(edge.0.borrow().name.clone(), None);
             ActResult::Ok
         },
         &mut HashSet::new(),
     );
-    costs.borrow_mut().insert(root.borrow().name, 0);
+    costs.borrow_mut().insert(root.borrow().name.clone(), 0);
 
     let nodes: &mut HashMap<NodeName, Link<T>> = &mut nodes.borrow_mut();
     let costs: &mut HashMap<NodeName, Weight> = &mut costs.borrow_mut();
     let parents: &mut HashMap<NodeName, Option<Link<T>>> = &mut parents.borrow_mut();
 
     while let Some(closest_node_name) = find_closest_node::<NodeName>(costs, &processed) {
-        let closest_node: Link<T> = nodes.get(closest_node_name).unwrap().clone();
-        let cost = *costs.get_mut(closest_node_name).unwrap();
+        let closest_node: Link<T> = nodes.get(&closest_node_name).unwrap().clone();
+        let cost = *costs.get_mut(&closest_node_name).unwrap();
         let edges = closest_node.borrow().edges.clone();
         for edge in edges {
-            let name = edge.0.borrow().name;
+            let name = edge.0.borrow().name.clone();
             let new_cost = cost + edge.1;
-            let old_cost = *costs.get(name).unwrap();
+            let old_cost = *costs.get(&name).unwrap();
             if old_cost > new_cost {
-                costs.insert(name, new_cost);
-                parents.insert(name, Some(closest_node.clone()));
+                costs.insert(name.clone(), new_cost);
+                parents.insert(name.clone(), Some(closest_node.clone()));
             }
         }
-        processed.insert(closest_node.borrow().name);
+        processed.insert(closest_node.borrow().name.clone());
     }
 
     (costs.clone(), parents.clone())
@@ -229,11 +229,11 @@ fn find_closest_node<T>(
     processed: &HashSet<NodeName>,
 ) -> Option<NodeName> {
     let mut closest_weight: Weight = Weight::MAX;
-    let mut closest_node_name: NodeName = "";
+    let mut closest_node_name: NodeName = "".to_string();
     for (k, v) in costs.iter() {
         if *v < closest_weight && !processed.contains(k) {
             closest_weight = *v;
-            closest_node_name = k;
+            closest_node_name = k.clone();
         }
     }
     if closest_node_name.is_empty() {
@@ -254,34 +254,37 @@ where
     root.borrow().traverse_breadth_first(
         &|edge| -> ActResult {
             *nodes_len.borrow_mut() += 1;
-            costs.borrow_mut().insert(edge.0.borrow().name, Weight::MAX);
-            parents.borrow_mut().insert(edge.0.borrow().name, None);
+            costs.borrow_mut().insert(edge.0.borrow().name.clone(), Weight::MAX);
+            parents.borrow_mut().insert(edge.0.borrow().name.clone(), None);
             ActResult::Ok
         },
         &mut HashSet::new(),
     );
     // From root to root weight is zero.
-    costs.borrow_mut().insert(root.borrow().name, 0);
+    costs.borrow_mut().insert(root.borrow().name.clone(), 0);
 
     for _ in 0..*nodes_len.borrow_mut() - 1 {
         root.borrow().traverse_breadth_first(
             &|edge| -> ActResult {
-                let parent_name = edge.0.borrow().name;
+                let parent_name = edge.0.borrow().name.clone();
                 let mut costs_ = costs.borrow_mut();
                 let mut parents_ = parents.borrow_mut();
                 // Parent cost from storage.
-                let g_parent_cost: Weight = *costs_.get(parent_name).unwrap();
+                let g_parent_cost: Weight = *costs_.get(&parent_name).unwrap();
 
                 // Check each child.
                 for child in edge.0.borrow().edges.iter() {
-                    let child_name = child.0.borrow().name;
+                    let child_name = child.0.borrow().name.clone();
                     // Child cost from storage.
-                    let g_child_cost: Weight = *costs_.get(child_name).unwrap();
+                    let g_child_cost: Weight = *costs_.get(&child_name).unwrap();
 
                     // Sum parent cost from storage with current child weight and compare with child cost in storage.
                     if g_parent_cost != Weight::MAX && g_parent_cost + child.1 < g_child_cost {
-                        costs_.insert(child_name, g_parent_cost + child.1);
-                        parents_.insert(child_name, Some(edge.0.clone()));
+                        costs_.insert(child_name.clone(), g_parent_cost + child.1);
+                        // TODO: Add same parent counter.
+                        //  As sometimes we have cycle or same path through all road we need to count same parents.
+                        //  Or maybe not to store just parent for every node but store full path?
+                        parents_.insert(child_name.clone(), Some(edge.0.clone()));
                     }
                 }
                 ActResult::Ok
@@ -300,6 +303,7 @@ where
 #[cfg(test)]
 mod tests {
     use crate::*;
+    use rand::Rng;
 
     #[test]
     fn test_depth_first_traverse() {
@@ -331,7 +335,7 @@ mod tests {
         println!("sorted");
         let mut nodes: Vec<NodeName> = Vec::new();
         for n in sorted {
-            nodes.push(n.borrow().name);
+            nodes.push(n.borrow().name.clone());
             print!("{} ", n.borrow().name)
         }
         println!();
@@ -351,14 +355,14 @@ mod tests {
         assert_eq!(
             costs,
             HashMap::from([
-                ("B", 9),
-                ("E", 14),
-                ("R", 0),
-                ("G", 4),
-                ("D", 11),
-                ("A", 1),
-                ("F", 22),
-                ("C", 7)
+                (String::from("B"), 9),
+                (String::from("E"), 14),
+                (String::from("R"), 0),
+                (String::from("G"), 4),
+                (String::from("D"), 11),
+                (String::from("A"), 1),
+                (String::from("F"), 22),
+                (String::from("C"), 7)
             ])
         );
         assert_eq!(*parents.get("R").unwrap(), None);
@@ -379,13 +383,13 @@ mod tests {
             Some(parents.get("G").unwrap().as_ref().unwrap().clone());
         while let Some(p) = parent {
             println!("{}", p.borrow().name);
-            parent = parents.get(p.borrow().name).unwrap().clone();
+            parent = parents.get(&p.borrow().name).unwrap().clone();
         }
     }
 
     #[test]
     fn test_bellman_ford() {
-        let root = gen_bellman_graph();
+        let root = gen_graph_bellman();
         root.borrow().traverse_breadth_first(&print_edge, &mut HashSet::new());
         let (costs, parents) = bellman_ford(root);
         println!("\ncosts - {:?}\n", costs);
@@ -397,15 +401,37 @@ mod tests {
         }
     }
 
+    #[test]
+    fn test_rand() {
+        let root = gen_graph_random(5, 3);
+        root.borrow().traverse_breadth_first(&print_edge, &mut HashSet::new());
+        let (costs, parents) = dijkstra(root.clone());
+        println!("{:?}", costs);
+        for p in parents {
+            if p.1.is_none() {
+                continue;
+            }
+            println!("parent for {} is {}", p.0, p.1.unwrap().borrow().name);
+        }
+        let (costs, parents) = bellman_ford(root);
+        println!("{:?}", costs);
+        for p in parents {
+            if p.1.is_none() {
+                continue;
+            }
+            println!("parent for {} is {}", p.0, p.1.unwrap().borrow().name);
+        }
+    }
+
     fn gen_graph() -> Link<u8> {
-        let r = Node::new("R", 0);
-        let a = Node::new("A", 1);
-        let b = Node::new("B", 2);
-        let c = Node::new("C", 3);
-        let d = Node::new("D", 4);
-        let e = Node::new("E", 5);
-        let f = Node::new("F", 6);
-        let g = Node::new("G", 7);
+        let r = Node::new(String::from("R"), 0);
+        let a = Node::new(String::from("A"), 1);
+        let b = Node::new(String::from("B"), 2);
+        let c = Node::new(String::from("C"), 3);
+        let d = Node::new(String::from("D"), 4);
+        let e = Node::new(String::from("E"), 5);
+        let f = Node::new(String::from("F"), 6);
+        let g = Node::new(String::from("G"), 7);
 
         r.borrow_mut().add_edge(Rc::clone(&a), 1).add_edge(Rc::clone(&b), 9);
         a.borrow_mut().add_edge(Rc::clone(&c), 6).add_edge(Rc::clone(&g), 3);
@@ -417,15 +443,15 @@ mod tests {
         r
     }
 
-    fn gen_bellman_graph() -> Link<u8> {
-        let r = Node::new("R", 0);
-        let a = Node::new("A", 1);
-        let b = Node::new("B", 2);
-        let c = Node::new("C", 3);
-        let d = Node::new("D", 4);
-        let e = Node::new("E", 5);
-        let f = Node::new("F", 6);
-        let g = Node::new("G", 7);
+    fn gen_graph_bellman() -> Link<u8> {
+        let r = Node::new(String::from("R"), 0);
+        let a = Node::new(String::from("A"), 1);
+        let b = Node::new(String::from("B"), 2);
+        let c = Node::new(String::from("C"), 3);
+        let d = Node::new(String::from("D"), 4);
+        let e = Node::new(String::from("E"), 5);
+        let f = Node::new(String::from("F"), 6);
+        let g = Node::new(String::from("G"), 7);
 
         r.borrow_mut().add_edge(Rc::clone(&a), 1).add_edge(Rc::clone(&b), 9);
         a.borrow_mut().add_edge(Rc::clone(&c), 6).add_edge(Rc::clone(&g), 3);
@@ -433,6 +459,37 @@ mod tests {
         c.borrow_mut().add_edge(Rc::clone(&d), 7);
         e.borrow_mut().add_edge(Rc::clone(&f), 8);
         f.borrow_mut().add_edge(Rc::clone(&a), -100).add_edge(Rc::clone(&g), 4);
+
+        r
+    }
+
+    // TODO: Add max depth.
+    fn gen_graph_random(max_nodes: u8, max_children: u8) -> Link<u8> {
+        let mut nodes: Vec<Link<u8>> = Vec::with_capacity(max_nodes as usize);
+
+        let r = Node::new(String::from("R"), 0);
+        nodes.push(r.clone());
+
+        for i in 0..max_nodes - 1 {
+            let node_name = i.to_string().clone();
+            let node = Node::new(node_name, 0);
+            nodes.push(node.clone());
+        }
+
+        let mut rng = rand::thread_rng();
+
+        for i in 0..max_nodes as usize - 1 {
+            let max_children_ = rng.gen_range(1..max_children + 1);
+            for _ in 0..max_children_ {
+                let rand_node = rng.gen_range(0..max_nodes) as usize;
+                let rand_weight: Weight = rng.gen_range(-10..10);
+                nodes
+                    .get(i)
+                    .unwrap()
+                    .borrow_mut()
+                    .add_edge(nodes.get(rand_node).unwrap().clone(), rand_weight);
+            }
+        }
 
         r
     }
